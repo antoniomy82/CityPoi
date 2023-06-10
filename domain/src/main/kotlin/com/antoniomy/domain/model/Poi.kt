cd996461a4a7577e40654676dde061702cb7c96a
Antonio Morales <antoniomy82@gmail.com>
2023-06-10 23:04:56 +0200

    Refactor

--- domain/src/main/kotlin/com/antoniomy/domain/model/Poi.kt
+++ domain/src/main/kotlin/com/antoniomy/domain/model/Poi.kt
@@ -1,14 +1,14 @@
 package com.antoniomy.domain.model
 
 data class Poi(
-    var image: String? = null, //URL
+    var image: String? = null,
     var latitude: Float? = null,
     var longitude: Float? = null,
-    var categoryIcon: String? = null, //ICON Y MARKER
-    var categoryMarker: String? = null, //ICON Y MARKER
+    var categoryIcon: String? = null,
+    var categoryMarker: String? = null,
     var name: String = "",
     var description: String? = null,
-    var audio: String? = null,  //URL
+    var audio: String? = null,
     var city: String? = null,
     var district: String? = null
 )
